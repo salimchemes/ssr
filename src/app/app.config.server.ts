24c1b89a05f5654158b073ad69d680a0374503b7
Salim Chemes <salimchemes@gmail.com>
2025-03-28 14:42:17 -0300

    fix: correct import statement for server routes in app configuration

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,7 +2,7 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideServerRouting } from '@angular/ssr';
-import { serverRoutes } from './app.routes.server.';
+import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
